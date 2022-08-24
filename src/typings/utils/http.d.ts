76d32136e1ff408ffa855ed6d07595e2d2342da7
汪嘉龙 <wangjialong@shiyue.com>
2022-08-24 16:15:34 +0800

    test eslint-autofix

--- src/typings/utils/http.d.ts
+++ src/typings/utils/http.d.ts
@@ -1,7 +1,7 @@
 import type { App } from 'vue'
 declare interface Http {
-  get(url: string, params: unknow): Promise<ResultType>
-  post(url: string, params: unknow): Promise<ResultType>
+  get(url: string, params: unknow):Promise<ResultType>
+  post(url: string, params: unknow):Promise<ResultType>
   put(url: string, params: unknow, content?: Array<string>): Promise<ResultType>
   upload(url: string, params: unknow): Promise<ResultType>
   delete(url: string): Promise<ResultType>

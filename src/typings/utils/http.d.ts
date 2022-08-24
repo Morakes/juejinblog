10bff80f744b00e1635f5582926c56258ce31685
汪嘉龙 <wangjialong@shiyue.com>
2022-08-24 16:17:00 +0800

    test: test autofix

--- src/typings/utils/http.d.ts
+++ src/typings/utils/http.d.ts
@@ -3,7 +3,7 @@ declare interface Http {
   get(url: string, params: unknow):Promise<ResultType>
   post(url: string, params: unknow):Promise<ResultType>
   put(url: string, params: unknow, content?: Array<string>): Promise<ResultType>
-  upload(url: string, params: unknow): Promise<ResultType>
+  upload(url: string, params: unknow):Promise<ResultType>
   delete(url: string): Promise<ResultType>
   download(url: string): void
   install(app: App): void
